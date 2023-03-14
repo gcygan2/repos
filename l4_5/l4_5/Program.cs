4cd879761221e957b35b44788f668857c8f6dd6b
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2023-03-14 18:39:32 +0100

    14032023

---
+++ l4_5/l4_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Gra> gry = new List<Gra>();
+            Gra umierajaceSwiatlo = new Gra ("Umierajace światło", 99.99m);
+            gry.Add(umierajaceSwiatlo);
+            
+        }
+    }
+}
