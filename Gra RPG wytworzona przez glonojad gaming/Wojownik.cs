f7ef574595cfe98c2bfdead4fe43b920cfeed5d7
gcygan2 <prezentacje@gcygan.webd.pl>
2023-03-22 11:53:19 +0100

    Michal

---
+++ Gra RPG wytworzona przez glonojad gaming/Wojownik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_RPG_wytworzona_przez_glonojad_gaming
+{
+    class wojownik : Hero
+    {
+        public wojownik(string Nazwa, int HP, int Atak) : base(Nazwa, HP, Atak)
+        {
+
+        }
+        int Obrona = 7;
+        public void odejmijHP()
+        {
+            int zyciedoodjecia = Atak - Obrona / 3;
+        }
+        public int Pobierzsileataku()
+        {
+            return Atak;
+        }
+    }
+}
